507d06ecdf03ff563505cdaa29055d4bf7c1add5
Debora Rodrigues <debora.rodrigues@vat.local>
2026-01-19 11:48:27 -0400

    Declaração de bibliotecas do router e navigate

--- site/src/routes/index.tsx
+++ site/src/routes/index.tsx
@@ -1,10 +1,14 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'; // This line is already correct.
+
+
 
 export const AppRoutes = ()=>{
   return(
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página Inicial</p>}>
-     <Route path="*"/ element={<Navigator/>}/>
+      <Route path="/pagina-inicial" element={<p>Página Inicial</p>}/>
+
+      <Route path="*" element={<Navigate to ="/pagina-inicial" />}/>
+  
       </Routes>
   );
 }
