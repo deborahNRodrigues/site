55d38b01273aa35ffab01dd02e9afb2afc7569e6
Debora Rodrigues <debora.rodrigues@vat.local>
2026-01-21 15:36:57 -0400

    Atualização de pastas e de imports para uso MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes , Route, Navigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/paginal-inicial" element={<p>PaginaInicial</p>} />
+            <Route path="/paginal-inicial" element={<Button>Página Inicial</Button>} />
 
             <Route path="*" element={<Navigate to="/paginal-inicial" />}/>
         </Routes>
